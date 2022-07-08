a6497e115d60ee9a9ce2c4a07c4be93d198b4367
RedEarSlider <hw3027@nyu.edu>
2022-07-07 21:34:45 -0400

    FirstAttempt

---
+++ my_script.py
@@ -0,0 +1,13 @@
+# Rock, Paper, Scissors Assignment 
+# by Helen Wang
+# 2022 Summer NYU
+# First Attempt
+print ("------------------")
+print ("Welcome 'Player One' to my Rock-Paper-Scissors game...") 
+print ("------------------")
+
+# Ask for Player One's input
+# print ("Please choose either 'rock', 'paper', or 'scissor'")
+YourChoice = input("Please choose either 'rock', 'paper', or 'scissor': ")
+print ("You chose:"+ YourChoice)
+
